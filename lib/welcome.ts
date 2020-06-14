cbf59056ddf949797bd3ef172b2269ab26c66aff
Shubham Batra <batrashubham@live.com>
2020-06-14 23:35:52 +0530

    convert welcome file to ts.

--- lib/welcome.ts
+++ lib/welcome.ts
@@ -1,15 +1,16 @@
-const chalk = require("chalk");
-const boxen = require("boxen");
+import chalk from "chalk";
+import boxen, { BorderStyle} from "boxen";
 
 const greeting = chalk.greenBright.bold("Terminal Market Watch _-^-_");
 
 const boxenOptions = {
     padding: 1,
     margin: 1,
-    borderStyle: "round",
-    borderColor: "green",
+    borderStyle: BorderStyle.Round,
+    borderColor: 'green',
     backgroundColor: "#555555",
     float: 'center'
 };
 
+// @ts-ignore
 export const WelcomeMessage = boxen( greeting, boxenOptions );
