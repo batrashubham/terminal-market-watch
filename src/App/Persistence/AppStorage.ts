233fbb4e3da5aee749ee296db143d8159fbe3542
Shubham <batrashubham@live.com>
2020-08-08 21:02:18 +0530

    remove sequelize

--- src/App/Persistence/AppStorage.ts
+++ src/App/Persistence/AppStorage.ts
@@ -1,26 +1,9 @@
-import { initializeWatchlist } from './Watchlist/Watchlist';
-import { Sequelize } from 'sequelize';
-import sqlite3 from 'sqlite3';
 import IStorage from './IStorage';
 import { injectable } from 'inversify';
 
 @injectable()
 export default class AppStorage implements IStorage {
-    private sequelizeInstance: Sequelize;
-
-    constructor() {
-        // const userDB = new sqlite3.Database('./user1.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
-        //     // do your thing
-        // });
-        this.sequelizeInstance = new Sequelize('sqlite::~/.tmw/tmw.db');
-        this.initialize();
-    }
-
-    getConnection(): Sequelize {
-        return this.sequelizeInstance;
-    }
-
-    private initialize(): void {
-        initializeWatchlist(this.sequelizeInstance);
+    getConnection(): void {
+        console.log('Get connection.');
     }
 }
--- src/App/Persistence/IStorage.ts
+++ src/App/Persistence/IStorage.ts
@@ -1,5 +1,3 @@
-import { Sequelize } from 'sequelize/types';
-
 export default interface IStorage {
-    getConnection(): Sequelize;
+    getConnection(): void;
 }
